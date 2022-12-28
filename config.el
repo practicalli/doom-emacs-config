71ab537cf45e6c6fe7ed3349397431c5138b4d56
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-12-28 15:53:28 +0000

    config: set relative line numbers for vim line jumps

--- config.el
+++ config.el
@@ -39,7 +39,7 @@
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
