f4fbc43a2607c07c3c014f329698cb6d8d21aa58
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-12-28 15:51:48 +0000

    config: set fira code 14 font

--- config.el
+++ config.el
@@ -23,7 +23,10 @@
 ;;
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
+
+(setq doom-font (font-spec :family "Fira Code" :size 14 :weight 'semi-light))
+
+
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
