62a3774b45a3a2c9926edc8b056a45648af0c49a
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-12-30 23:45:37 +0000

    module: use magit with forge

--- init.el
+++ init.el
@@ -96,8 +96,8 @@
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        lsp               ; M-x vscode
-       magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
+       (magit +forge)             ; a git porcelain for Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
