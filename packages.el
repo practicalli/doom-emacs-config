a0d52c87ac85effcb8b274ef5a89d9c2d49fa093
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-01-01 16:51:22 +0000

    packages: add kaocha-runner package
    Install kaocha-runner from melpa (this package is not included in any
    doom module)

--- packages.el
+++ packages.el
@@ -48,3 +48,8 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+
+
+;; Koacha test runner in Emacs
+(package! kaocha-runner)
