fcc78355f815849601a935151241856cab7ba698
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-01-02 13:50:06 +0000

    module: add unicode support for more characters

--- init.el
+++ init.el
@@ -53,7 +53,7 @@
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        (treemacs +lsp)          ; a project drawer, like neotree but cooler
-       ;;unicode           ; extended unicode support for various languages
+       unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
        ;; vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
