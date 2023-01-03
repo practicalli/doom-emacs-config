3e8fdb6230193bcc0f2a6dc9eb3dd401793bb94b
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-01-03 21:59:42 +0000

    lsp: clean up configuration, remove local lsp server

--- +lsp.el
+++ +lsp.el
@@ -1,6 +1,5 @@
 ;;; +lsp.el -*- lexical-binding: t; -*-
 
-
 ;; ---------------------------------------
 ;; LSP Configuration
 
@@ -16,16 +15,7 @@
         lsp-idle-delay 0.2 ;; Smoother LSP features response in cost of performance (Most servers I use have good performance)
         lsp-use-plists nil)
   (add-hook 'lsp-after-apply-edits-hook (lambda (&rest _) (save-buffer)))
-  (add-hook 'lsp-mode-hook (lambda () (setq-local company-format-margin-function #'company-vscode-dark-icons-margin)))
-
-  ;; Clojure LSP local development
-  ;; (let ((clojure-lsp-dev (expand-file-name "~/dev/clojure-lsp/clojure-lsp")))
-  ;;   (when (file-exists-p clojure-lsp-dev)
-  ;;     ;; clojure-lsp local development
-  ;;     (setq lsp-clojure-custom-server-command `("bash" "-c" ,clojure-lsp-dev)
-  ;;           lsp-completion-no-cache t
-  ;;           lsp-completion-use-last-result nil)))
-  )
+  (add-hook 'lsp-mode-hook (lambda () (setq-local company-format-margin-function #'company-vscode-dark-icons-margin))))
 
 
 (use-package! lsp-treemacs
