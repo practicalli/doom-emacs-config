08c699c5b262a3845604de31e765be8276bf08fa
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-02-11 23:40:38 +0000

    init: enable lookup of words in online dictionary

--- init.el
+++ init.el
@@ -102,7 +102,7 @@
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
-       lookup              ; navigate your code and its documentation
+       (lookup +dictionary)             ; navigate your code and its documentation
        (lsp +peek)              ; M-x vscode
        (magit +forge)             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
